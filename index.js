608e469494158c3e65f15b966eeee1854e4f2a38
jjhannes <jj.hannes.swanepoel@gmail.com>
2024-04-16 09:20:54 +0200

    complete recipe calculation api functionality - call potion mediator from potion controller - log all incoming request methods and paths

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const url = `http://localhost:${port}/`;
 
 let app = express();
 
+app.use((request, response, next) => {
+    console.log(`${request.method} ${request.path}`);
+    next();
+});
+
 potionsController.createEndpoints(app);
 
 app.listen(6667, () => console.log(`Potion helper running at ${url}`))
--- potions-controller.js
+++ potions-controller.js
@@ -1,11 +1,14 @@
 
-const potions = require("./potions-mediator");
+const potionsMediator = require("./potions-mediator");
 
 function createEndpoints(app) {
     app.get("/neapi/v1/potions/recipes/with-effects", (request, response) => {
-        console.log(`${request.query}`);
+        let desiredEffects = request.query["de"]
+            .split(",")
+            .map(de => de.trim());
+        let viableRecipes = potionsMediator.determineRecipe(desiredEffects);
 
-        response.send([]);
+        response.send(viableRecipes);
     });
 }
 
